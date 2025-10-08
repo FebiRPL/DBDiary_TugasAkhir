8f6c2ebf1b2f9f6bdd0eb62eef260eb22248246d
FebiRpl <febprogrammer@gmail.com>
2025-10-08 14:07:10 +0700

    Penambahdan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.9.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
