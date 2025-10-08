a976a0a1f19c76f60b15d7e68e0335a7e6402963
FebiRpl <febprogrammer@gmail.com>
2025-10-08 14:50:14 +0700

    Penambahdan

--- app/src/main/java/com/example/dbdiary_tugasakhir/Diary.kt
+++ app/src/main/java/com/example/dbdiary_tugasakhir/Diary.kt
@@ -10,7 +10,7 @@ class Diary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_diary)
+        setContentView(R.layout.item)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
